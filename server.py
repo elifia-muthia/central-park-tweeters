8c56035639338dce283ecb2d10ff524565764788
Elifia Muthia <elifiamuthia@Elifias-MacBook-Air.local>
2024-04-16 13:53:10 -0400

    set up basic architecture

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+from flask import Response, request, jsonify
+app = Flask(__name__)
+
+# ROUTES
+@app.route('/')
+def load_homepage():
+   return render_template('homepage.html')
+
+@app.route('/quiz')
+def load_quiz():
+   return render_template('quiz.html')
+
+# AJAX FUNCTIONS
