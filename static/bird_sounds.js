4bb63ce5bbd3c63476ce9e3d470e5bbfa5ffe867
Elifia Muthia <elifiamuthia@Elifias-MacBook-Air.local>
2024-04-17 12:51:06 -0400

    made birds sounds page

--- static/bird_sounds.js
+++ static/bird_sounds.js
@@ -5,19 +5,24 @@ $(document).ready(function() {
 
 function show_results(results) {
     $.each(results, function(index, bird) {
-        var row = $('<tr>')
-            .attr('data-id', bird.id) 
-            .append(
-                $('<td>').text(bird.name),
-                $('<td>').text(bird.season)
-            )
-            .click(function() {
-                window.location.href = '/view/' + $(this).data('id');
-            });
-    
-        $('table tbody').append(row);
-    });
-    
-   
+        console.log(bird)
+        // Create a container for each bird
+        var birdDiv = $('<div>').addClass('bird-tone');
 
+        // Add the title
+        birdDiv.append($('<h3>').text(bird.title));
+
+        // Add the audio player if a sound file is available
+        if (bird.sound) {
+            var audioHtml = '<audio controls><source src="' + bird.sound + '" type="audio/mpeg">Your browser does not support the audio element.</audio>';
+            birdDiv.append($(audioHtml));
+        }
+
+        if (bird.info) {
+            birdDiv.append($('<p>').text(bird.info));
+        }
+
+        // Append this bird's div to the main container
+        $('#tone').append(birdDiv);
+    });
 }
